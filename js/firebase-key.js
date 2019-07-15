0a0ad21951787b0b118d4c433d0c8af8e1d0807a
juan6218 <diazj329@coyote.csusb.edu>
2019-07-15 14:45:49 -0700

    Update firebase-key.js

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
   "apiKey": 'AIzaSyBff0KCstNSBQI5C8BNcO5DuHnMEyILgVk', 
-  "authDomain": 'table-champ-work.firebaseapp.com', 
+  "authDomain": 'tablechamp.netlify.com', 
   "databaseURL": 'https://table-champ-work.firebaseio.com' 
 };
