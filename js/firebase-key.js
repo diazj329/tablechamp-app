22be98408aa26920d67480ceaf6dcdd99a0a9228
juan6218 <diazj329@coyote.csusb.edu>
2019-07-15 14:48:36 -0700

    revert

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
   "apiKey": 'AIzaSyBff0KCstNSBQI5C8BNcO5DuHnMEyILgVk', 
-  "authDomain": 'tablechamp.netlify.com', 
+  "authDomain": '6f71687c59c0b133de26d8f6ee8c34f74d86637b', 
   "databaseURL": 'https://table-champ-work.firebaseio.com' 
 };
