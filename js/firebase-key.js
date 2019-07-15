6f71687c59c0b133de26d8f6ee8c34f74d86637b
juan6218 <diazj329@coyote.csusb.edu>
2019-07-15 13:50:48 -0700

    Update firebase-key.js

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyBff0KCstNSBQI5C8BNcO5DuHnMEyILgVk', 
+  "authDomain": 'table-champ-work.firebaseapp.com', 
+  "databaseURL": 'https://table-champ-work.firebaseio.com' 
 };
