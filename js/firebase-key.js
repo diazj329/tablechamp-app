61ea46f3cbbd3e471ebc628427776623d3274801
juan6218 <diazj329@coyote.csusb.edu>
2019-07-15 14:49:28 -0700

    revert

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
   "apiKey": 'AIzaSyBff0KCstNSBQI5C8BNcO5DuHnMEyILgVk', 
-  "authDomain": '6f71687c59c0b133de26d8f6ee8c34f74d86637b', 
+  "authDomain": 'table-champ-work.firebaseapp.com', 
   "databaseURL": 'https://table-champ-work.firebaseio.com' 
 };
